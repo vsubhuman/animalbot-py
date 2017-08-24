9f45ebc477a3a7099a98c4b2bf1720e2cf598f3b
vsubhuman <vsubhuman@yandex-team.ru>
2017-08-24 14:12:37 +0300

    Added basic echo functionality

--- src/animalbot.py
+++ src/animalbot.py
@@ -1,11 +1,26 @@
 import requests
+import os
+
+TOKEN = os.environ['bot_token']
 
 
-def req():
-    requests.post("https://api.telegram.org/bot379978390:AAHk9QTeogqdUp3waYPSMCMV6ApKYiGR5_4/sendMessage",
-                  data={'chat_id': 256295697, 'text': 'Python test'})
+def post(method, body):
+    print("Posting '%s' with data: %s" % (method, body))
+    r=requests.post("https://api.telegram.org/bot%s/%s" % (TOKEN, method), data=body)
+    print("Response from '%s' is {code=%s, reason=%s}: %s" % (method, r.status_code, r.reason, r.text))
+
+
+def send_message(chat_id, text):
+    post('sendMessage', {'chat_id': chat_id, 'text': text})
 
 
 def handler(evt, ctx):
-    print(evt)
-
+    print("Event: %s" % evt)
+    msg=evt['message']
+    if msg:
+        print('Processing message')
+        chat=msg['chat']
+        text=msg['text']
+        user=msg['from']
+        if text and (chat['type'] == 'private') and (user['username'] == 'vsubhuman'):
+            send_message(chat['id'], "Echo> %s" % text)
