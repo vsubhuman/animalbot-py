78061295f3dc34bf5b79e6ae80de1943b6807e02
vsubhuman <vsubhuman@yandex-team.ru>
2017-08-24 21:34:10 +0300

    Extracted module with telegram bot and added ignore in test environment

--- src/animalbot.py
+++ src/animalbot.py
@@ -1,26 +1,25 @@
-import requests
 import os
 
-TOKEN = os.environ['bot_token']
+import telegram
 
-
-def post(method, body):
-    print("Posting '%s' with data: %s" % (method, body))
-    r=requests.post("https://api.telegram.org/bot%s/%s" % (TOKEN, method), data=body)
-    print("Response from '%s' is {code=%s, reason=%s}: %s" % (method, r.status_code, r.reason, r.text))
-
-
-def send_message(chat_id, text):
-    post('sendMessage', {'chat_id': chat_id, 'text': text})
+version = '1.0-SNAPSHOT'
+bot = telegram.bot(os.environ['bot_token'])
+environment = os.environ.get('environment', 'test')
 
 
 def handler(evt, ctx):
     print("Event: %s" % evt)
-    msg=evt['message']
+    msg = evt.get('message')
     if msg:
         print('Processing message')
-        chat=msg['chat']
-        text=msg['text']
-        user=msg['from']
-        if text and (chat['type'] == 'private') and (user['username'] == 'vsubhuman'):
-            send_message(chat['id'], "Echo> %s" % text)
+        chat = msg['chat']
+        user = msg['from']
+        text = msg.get('text')
+        if environment == 'test':
+            if chat['type'] != 'private':
+                bot.leave_chat(chat['id'])
+                return
+            elif user['username'] != 'vsubhuman':
+                return
+        if text:
+            bot.send_message(chat['id'], "Echo> %s" % text)
---
+++ src/telegram/__init__.py
@@ -0,0 +1,30 @@
+import requests
+
+
+class Bot:
+    token = None
+
+    def __init__(self, token):
+        self.token = token
+
+    def send(self, method, body):
+        print("Posting '%s' with data: %s" % (method, body))
+        r = requests.post("https://api.telegram.org/bot%s/%s" % (self.token, method), data=body)
+        print("Response from '%s' is {code=%s, reason=%s}: %s" % (method, r.status_code, r.reason, r.text))
+        return r
+
+    def send_message(self, chat_id, text):
+        self.send('sendMessage', {'chat_id': chat_id, 'text': text})
+
+    def send_photo(self, chat_id, photo, text=None):
+        self.send('sendPhoto', {'chat_id': chat_id, 'photo': photo, 'caption': text})
+
+    def send_video(self, chat_id, video, text=None):
+        self.send('sendPhoto', {'chat_id': chat_id, 'video': video, 'caption': text})
+
+    def leave_chat(self, chat_id):
+        self.send('leaveChat', {'chat_id': chat_id})
+
+
+def bot(token):
+    return Bot(token)
